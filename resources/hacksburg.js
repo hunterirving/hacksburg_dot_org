a085c000389a30cf6e690da756cd7479a120517a
Hunter <hunter@hunterirving.com>
2022-12-23 16:36:01 -0500

    dim current_page in nav_links while another page is hovered

--- resources/hacksburg.js
+++ resources/hacksburg.js
@@ -1,3 +1,21 @@
-/* onload...
-		for each nav_link that is not the current_page
-			add onhover="dim_current_page()"
+window.addEventListener('DOMContentLoaded', (event) => {
+	current_page = document.getElementById('current_page');
+	console.log(current_page);
+	nav_links = document.getElementById('nav_links').children;
+	for (var i = 0; i < nav_links.length; i++) {
+		if (nav_links[i].id != "nav_break" && nav_links[i].id != "current_page") {
+			nav_links[i].onmouseover = dim_current_page;
+			nav_links[i].onmouseout = undim_current_page;
+		}
+	}
+});
+
+
+function dim_current_page() {
+	current_page.style.cssText = "color: var(--black)";
+}
+
+function undim_current_page() {
+	current_page.style.cssText = "color: var(--darker_green)";
+}
+

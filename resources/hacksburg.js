8ae51ec5cb502456db2a37d9ec77b9e4a124fa6d
Hunter <hunter@hunterirving.com>
2022-12-23 22:25:26 -0500

    after hovering over a nav_item, disable current_page highlighting until leaving #nav_links

--- resources/hacksburg.js
+++ resources/hacksburg.js
@@ -1,15 +1,14 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 	current_page = document.getElementById('current_page');
-	console.log(current_page);
-	nav_links = document.getElementById('nav_links').children;
+	nav_link_container = document.getElementById('nav_links')
+	nav_links = nav_link_container.children;
+	nav_link_container.onmouseleave = undim_current_page;
 	for (var i = 0; i < nav_links.length; i++) {
 		if (nav_links[i].id != "nav_break" && nav_links[i].id != "current_page") {
 			nav_links[i].onmouseover = dim_current_page;
-			nav_links[i].onmouseout = undim_current_page;
 		}
 	}
 });
-
 
 function dim_current_page() {
 	current_page.style.cssText = "color: var(--black)";

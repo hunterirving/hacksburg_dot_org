5a81e3153bbdff0770df615a37ab9947f5d384b8
Hunter <hunter@hunterirving.com>
2022-12-28 23:44:19 -0500

    first pass at object-fit: cover for post_images

--- resources/hacksburg.js
+++ resources/hacksburg.js
@@ -29,21 +29,5 @@ function togglePostOpened(x)
 		x.style.transform = "rotate(0deg)";
 		post_body.classList.add('closed'); 
 	}
-	/*if (!isHidden)
-	{
-		plus.style.transform = 'rotate(0deg)';
-		nav_links.style.opacity = "0%";
-		nav_links.style.maxHeight = "0px";
-		//nav_links.style.paddingBottom = "35px";
-		nav_links.classList.add('hidden');
-	}
-	else
-	{
-		plus.style.transform = 'rotate(-45deg)';
-		nav_links.style.opacity = "100%";
-		nav_links.style.maxHeight = "50px";
-		//nav_links.style.paddingBottom = "50px";
-		nav_links.classList.remove('hidden');
-	}*/
 }
 
